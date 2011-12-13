8602bc58341a8a0591ed0d40b75b14a54c762b77
Dmitry Suvorov <suvdim@gmail.com>
2011-12-14 00:18:32 +0400

    #198: added basic calendar workspace view

---
+++ src/zojax/calendar/browser/calendar.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id$
+"""
+from zope.component import getUtility
+
+from zojax.catalog.interfaces import ICatalog
+
+
+class ClendarView(object):
+
+    hasEvents = True
+
+    def update(self):
+        context = self.context
+        request = self.request
+
+        catalog = getUtility(ICatalog)
+
+        results = catalog.searchResults(
+            traversablePath = {'any_of': (context,)})
+
+        if not results:
+            self.hasEvents = False
+            return
+
+        self.events = results
